6b6ce1df2afa1bbd226f0e0ca052c82c72b68483
paolobettelini <paolo.bettelini@samtrevano.ch>
2023-07-29 15:05:47 +0200

    Initialized actix server

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,6 +1,9 @@
-mod args;
-mod handler;
+use actix_files::Files;
+use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
+use env_logger;
+use std::path::Path;
 
+mod args;
 use args::*;
 
 #[macro_use]
@@ -10,9 +13,33 @@ lazy_static! {
     pub static ref CONFIG: Args = Args::parse();
 }
 
+// &CONFIG.www, &CONFIG.data
 #[tokio::main]
-async fn main() {
-    let routes = handler::get_routes(&CONFIG.www, &CONFIG.data);
+async fn main() -> std::io::Result<()> {
+    // Initialize logging
+    env_logger::init();
 
-    warp::serve(routes).run((CONFIG.address, CONFIG.port)).await;
+    HttpServer::new(|| {
+        App::new()
+            .service(courses)
+            // Static files
+            .service(Files::new("/", &CONFIG.www).index_file("index.html"))
+    })
+    .bind((CONFIG.address, CONFIG.port))?
+    .run()
+    .await
+}
+
+#[post("/course/{course}")]
+async fn courses(course: web::Path<String>) -> impl Responder {
+    let file_name = format!("{course}.json");
+    log::debug!("Reading file: {file_name:?}");
+    // TODO pre-create path
+    let file = &Path::new(&CONFIG.data).join("courses").join(file_name);
+    let content = std::fs::read_to_string(file).unwrap();
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(content)
+    // HttpResponse::NotFound().body("Course not found")
 }
