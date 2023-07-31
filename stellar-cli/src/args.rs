d4eeec881a3298ab9284b53c6800cb6ee3015378
paolobettelini <paolo.bettelini@samtrevano.ch>
2023-07-31 23:53:37 +0200

    cli parameters

--- stellar-cli/src/args.rs
+++ stellar-cli/src/args.rs
@@ -5,13 +5,30 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// LaTex file
-    #[arg(short, long, requires = "data_output")]
-    #[arg(short = 'i', long)]
+    /// Generate data from a LaTex file
+    #[arg(short = 'i', long, requires = "data_output")]
     pub latex_input: Option<PathBuf>,
 
-    /// Output director for .tex files
-    #[arg(short, long, requires = "latex_input")]
-    #[arg(short = 'o', long)]
+    /// Output for generated data (uncompiled .tex files)
+    #[arg(short = 'o', long, requires = "latex_input")]
     pub data_output: Option<PathBuf>,
+
+    /// MongoDB connection URL
+    pub connection_url: Option<String>,
+
+    /// Import data folder
+    #[arg(requires = "connection_url")]
+    pub import_data: Option<PathBuf>,
+
+    /// Import snippets folder
+    #[arg(requires = "connection_url")]
+    pub import_snippets: Option<PathBuf>,
+
+    /// Import pages folder
+    #[arg(requires = "connection_url")]
+    pub import_pages: Option<PathBuf>,
+
+    /// Import courses folder
+    #[arg(requires = "connection_url")]
+    pub import_courses: Option<PathBuf>,
 }
