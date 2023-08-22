3eef03c0ac011dc0edcc7dec97052399b259d636
paolobettelini <paolo.bettelini@samtrevano.ch>
2023-08-22 16:42:35 +0200

    Created stellar-compile lib

--- crates/stellar-import/src/lib.rs
+++ crates/stellar-import/src/lib.rs
@@ -4,6 +4,7 @@ mod import;
 
 pub use import::*;
 
+// Maybe this should be in stellar-database
 pub async fn get_client(connection_url: &str) -> anyhow::Result<ClientHandler> {
     let client = ClientHandler::new(connection_url).await?;
     //client.create_indexes();
