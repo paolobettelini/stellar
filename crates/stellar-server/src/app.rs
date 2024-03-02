1c875de4d82feff345ee8a56ef304b4a1067b58e
paolobettelini <paolo.bettelini@samtrevano.ch>
2024-03-02 15:55:40 +0100

    using thaw

--- crates/stellar-server/src/app.rs
+++ crates/stellar-server/src/app.rs
@@ -1,6 +1,7 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use thaw::*;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -31,12 +32,10 @@ pub fn App() -> impl IntoView {
 #[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
         <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <Button variant=ButtonVariant::Primary>"Primary"</Button>
     }
 }
 
