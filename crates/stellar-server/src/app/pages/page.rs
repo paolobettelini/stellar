10fe455dde1fc03df92e95b7117de6aa96e5e86e
paolobettelini <paolo.bettelini@samtrevano.ch>
2024-06-29 15:46:46 +0200

    /page page

--- crates/stellar-server/src/app/pages/page.rs
+++ crates/stellar-server/src/app/pages/page.rs
@@ -1,14 +1,20 @@
 use leptos::*;
-
+use leptos_router::use_params_map;
 use crate::app::{Navbar, PageRenderer, Topbar};
 
 #[component]
 pub fn PagePage() -> impl IntoView {
     let page_sig = create_rw_signal("".to_string());
     let page = page_sig.read_only();
-    let (title, set_title) = create_signal("".to_string());
+    
+    let params = use_params_map();
+    let page_v = params.with(|params| params.get("page").cloned().unwrap_or_default());
+
+    page_sig.set(page_v.clone());
 
     view! {
+        <h1>Page ID: {page_v}</h1>
+        <hr></hr>
         <PageRenderer page />
     }
 }
